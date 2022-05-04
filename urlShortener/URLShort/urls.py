6d7c9f34ebd08f5a585a5ed1fea1df6b29ff6622
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-05-04 12:04:06 +0100

    if output is nothing show enter a shortened url

--- urlShortener/URLShort/urls.py
+++ urlShortener/URLShort/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('home/', views.Home, name = 'url-home')
+    path('home/', views.Home, name = 'url-home'),
+    path('shorten/', views.Shorten, name = 'url-Shorten')
 
 ]
 
--- urlShortener/URLShort/views.py
+++ urlShortener/URLShort/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 # Create your views here.
 
 def Home(request):
     return render(request, 'base.html')
+
+@csrf_exempt
+def Shorten(request):
+    if request.method == 'POST':
+        response = request.POST.get('inputurl')
+        print(response)
+        urloutput = {'output': ""}
+        return render(request, 'shorten.html', context = urloutput)
+    else:
+        return render(request, 'shorten.html')
