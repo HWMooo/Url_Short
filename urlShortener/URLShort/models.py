17c959cd0ef913c72f59e7d5802e1c876bdd8d96
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-05-04 12:45:35 +0100

    added model and shorten url function

--- urlShortener/URLShort/models.py
+++ urlShortener/URLShort/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import pyshorteners as pys
 
 # Create your models here.
+class UrlToShort(models.Model):
+    inputurl = models.TextField(max_length=1000, blank=False)
+    outputurl = models.TextField(max_length=500, blank=False)
+
+    def shortenurl(self, inputurl):
+        type_tiny = pys.Shortener()
+        outputurl = type_tiny.tinyurl.short(inputurl)
+        return outputurl
+
--- urlShortener/URLShort/views.py
+++ urlShortener/URLShort/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from . import models
 
 
 
@@ -12,8 +13,11 @@ def Home(request):
 def Shorten(request):
     if request.method == 'POST':
         response = request.POST.get('inputurl')
+        url = models.UrlToShort()
+        url.inputurl = response
+        url.outputurl = url.shortenurl(response)
         print(response)
-        urloutput = {'output': ""}
+        urloutput = {'output': url.outputurl}
         return render(request, 'shorten.html', context = urloutput)
     else:
         return render(request, 'shorten.html')
